3a33f55cce359f06eb84fa3977949afd814a7771
romingoon <romingoon@gmail.com>
2023-08-07 15:31:26 +0900

    sass 스타일시트 적용법 실습

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: [{ find: '~', replacement: `${__dirname}/src` }],
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: '@import "./src/scss/_variables";',
+      },
+    },
+  },
+});
