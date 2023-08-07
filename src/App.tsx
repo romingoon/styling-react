5bf598e81106adea811358692894854d7cde6fdd
romingoon <romingoon@gmail.com>
2023-08-07 15:36:51 +0900

    CSS Module 방식 사용법 실습 (1)

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import { Component } from 'react';
-import SassComponent from './Sass.Component';
+import CSSModule from './CSSModule';
 
 class App extends Component {
   render() {
     return (
       <div>
-        ddd
-        <SassComponent />
+        <CSSModule />
       </div>
     );
   }
